c57ffad8a23a23d515f3ad4e095a0cdf93638aad
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-14 19:19:49 -0500

    Devised ugly solution.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,16 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    original = None
+
+    for item in arr:
+        clone = arr.count(item)
+        if clone == 1:
+            original = item
+    return original
 
 
 if __name__ == '__main__':
