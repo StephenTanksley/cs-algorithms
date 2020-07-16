5ff6405b7ed16ad7f3de6deadabac7491444c121
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-16 11:02:16 -0500

    Worked on some optimization UPER.

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,35 +1,62 @@
+from functools import reduce
+
 '''
 Input: a List of integers
 Returns: a List of integers
 '''
 
 """
-U - We're given a list of integers. 
-    We're required to return the product
-    of each number by every OTHER number 
-    in the list. We're required to do this on O(n) time
-    AND O(n) space without using division.
+U - We're given a list of integers.  We're required to 
+    return the product of each number by every OTHER number 
+    in the list. We're required to do this in O(n) time
+    AND O(n) space without using division. 
+    
+    To do this with division, you could just get the product 
+    of the entire array and divide it by the index that should 
+    be skipped. For our solution, we're going to remove that 
+    value entirely.
     
 P - Naive solution: 
     
     1) create a temporary array and a solutions array
-    2) create a loop
-        2a) Copy the array into the temporary array
+    2) create a loop and copy the array into the temporary array
         3) for an item at i, remove it from the temporary array
         4) using a lambda function, get the product of the entire array minus the item
         5) push that value into the solutions array
     6) return solutions array
+    
+P - Optimized(?) solution
+    
+    1) Pretty much do the same thing as on the Naive solution.
+    2) Cache the results of the first function call and the popped value using enum.
+    3) Compare the popped value to value at the loop index.
+        4) if they're the same, push the results of that prior function call into 
+           solutions array and keep the popped value the same.
+        5) if they're different, perform caching of the new value
 
 """
 
 
 def product_of_all_other_numbers(arr):
 
+    # We create a solutions array to store our final solutions.
     solutions = []
-    temp = arr[:]
-    for i in temp:
+    temp = []
+
+    # We create a temporary copy of the original array. We only want to do operations on this temporary copy.
+    # We want to create the temporary copy of the array inside the loop because we don't want the copy of the array to persist beyond one cycle.
+    for i, num in enumerate(arr):
+        temp = arr[:]
+        # we pop off the value at the index of i
         popped = temp.pop(i)
-        print('popped', popped)
+
+        # we reduce the temporary array down to a single product
+        product = reduce((lambda x, y: x * y), temp)
+
+        # we add that product to our solutions accumulator array and repeat the loop
+        solutions.append(product)
+
+    return solutions
 
 
 if __name__ == '__main__':
